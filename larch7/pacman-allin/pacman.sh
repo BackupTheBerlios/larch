66329d5f6cf45a171daa8d694991cd5fab493e06
Michael Towers <larch42@googlemail.com>
2010-03-04 09:02:01 +0100

    pacman-allin: I think I made a bit of a botch with updating this the last time.

--- larch7/pacman-allin/pacman.sh
+++ larch7/pacman-allin/pacman.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 FULLPATH="$( readlink -f $0 )"
-SCRIPTDIR="$( dirname ${FULLPATH} )"
-LIBDIR="$( dirname ${SCRIPTDIR} )/lib"
+LIBDIR="$( dirname ${FULLPATH} )/lib"
 $LIBDIR/loader --library-path $LIBDIR $LIBDIR/pacman $*
